c066cde7fb543bf0b8db0117da9dbd60cb23abd5
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-05-08 03:53:24 +0900

    feat: configure env variables

--- apps/nestar-batch/src/nestar-batch.module.ts
+++ apps/nestar-batch/src/nestar-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestarBatchController } from './nestar-batch.controller';
 import { NestarBatchService } from './nestar-batch.service';
+import { ConfigModule } from '@nestjs/config/dist';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [NestarBatchController],
   providers: [NestarBatchService],
 })
