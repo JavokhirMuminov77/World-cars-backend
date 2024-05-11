447fa7bb92592f9611cf9f55308fce749888d5e1
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-05-11 20:58:39 +0900

    98.3

--- train.ts
+++ train.ts
@@ -1,39 +1,9 @@
-// function reduceNestedArray(arr: any[]): number {
-//   let total: number = 0;
-//   for (const element of arr) {
-//       if (Array.isArray(element)) {
-//           total += reduceNestedArray(element);
-//       } else if (typeof element === 'number') {
-//           total += element;
-//       }
-//   }
-//   return total;
-// }
-// console.log("Train");
-// // Misol uchun:
-// console.log(reduceNestedArray([1, [1, 2, [4]]]));
-
-
-
-// function reduceNestedArray(arr: (number | any)[]): number {
-// 	return arr.reduce((acc, curr) => {
-// 		if (typeof curr === 'number') {
-// 			return acc + curr;
-// 		} else if (Array.isArray(curr)) {
-// 			return acc + reduceNestedArray(curr);
-// 		} else {
-// 			return acc;
-// 		}
-// 	}, 0);
-// }
-
-// console.log("Train =>",reduceNestedArray([1, [1, 2, [4]]]));
 
 
 // ZK-TASK:
 
 function printNumbers() {
-	
+
 	const interval = setInterval(() => {
 		for (let i = 1; i <= 5; i++) {
 			console.log(i);
