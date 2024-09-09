8e269d4cb7a7940581d6d9a06b4373d663c6877c
JavokhirMuminov77 <javokhirmuminovdev@gmail.com>
2024-09-09 20:32:23 +0900

    feat

--- apps/world-cars-app/src/libs/interceptor/Loggin.interceptor.ts
+++ apps/world-cars-app/src/libs/interceptor/Loggin.interceptor.ts
@@ -1,35 +1,38 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
-import { Observable, tap } from 'rxjs';
-// import { Observable } from 'rxjs';
-// import { tap } from 'rxjs/operators';
+import { stringify } from 'querystring';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
 	private readonly logger: Logger = new Logger();
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const recordTime = Date.now();
+    const requestType = context.getType<GqlContextType>();
 
-	public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-		const recordTime = Date.now();
-		const requestType = context.getType<GqlContextType>();
+    if (requestType === 'graphql') {
+        const gqlContext = GqlExecutionContext.create(context);
+        const reqBody = gqlContext.getContext().req.body || {};
+        this.logger.log(`${this.stringify(reqBody)}`, 'REQUEST');
 
-    if (requestType === 'http') {
-      /* Develop if needed! */
-    } else if (requestType === 'graphql') {
-      /* (1) Print Request */
-      const gqlContext = GqlExecutionContext.create(context);
-      this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
-      /* (2) Errors handing via GraphQL */
+        // Check the observable returned by next.handle()
+        const observable = next.handle();
+        if (observable === undefined) {
+            this.logger.error('The observable returned by next.handle() is undefined');
+        }
 
-      /* (3) No Errors, giving Response below */
-      return next.handle().pipe(tap((context) => {
-        const responseTime = Date.now() - recordTime;
-          this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'RESPONSE')
-      }),
-    );
+        return observable.pipe(
+            tap((context) => {
+                const responseTime = Date.now() - recordTime;
+                this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'RESPONSE');
+            }),
+        );
+    } else {
+        return next.handle(); // For other request types
     }
-		
-	}
-  private stringify(context: ExecutionContext): string {
-    return JSON.stringify(context).slice(0, 75);
+}
+  stringify(reqBody: any) {
+    throw new Error('Method not implemented.');
   }
 }
